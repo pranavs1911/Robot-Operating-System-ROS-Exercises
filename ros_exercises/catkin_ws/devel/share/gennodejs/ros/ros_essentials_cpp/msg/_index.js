7582d2e2c0f4397566311d42bc09d081f3593d7d
Pranav Shankar S <spranav1911@gmail.com>
2022-04-01 09:37:28 +0530

    Assignments in ROS

---
+++ ros_exercises/catkin_ws/devel/share/gennodejs/ros/ros_essentials_cpp/msg/_index.js
@@ -0,0 +1,22 @@
+
+"use strict";
+
+let IoTSensor = require('./IoTSensor.js');
+let FibonacciActionResult = require('./FibonacciActionResult.js');
+let FibonacciResult = require('./FibonacciResult.js');
+let FibonacciActionGoal = require('./FibonacciActionGoal.js');
+let FibonacciAction = require('./FibonacciAction.js');
+let FibonacciFeedback = require('./FibonacciFeedback.js');
+let FibonacciActionFeedback = require('./FibonacciActionFeedback.js');
+let FibonacciGoal = require('./FibonacciGoal.js');
+
+module.exports = {
+  IoTSensor: IoTSensor,
+  FibonacciActionResult: FibonacciActionResult,
+  FibonacciResult: FibonacciResult,
+  FibonacciActionGoal: FibonacciActionGoal,
+  FibonacciAction: FibonacciAction,
+  FibonacciFeedback: FibonacciFeedback,
+  FibonacciActionFeedback: FibonacciActionFeedback,
+  FibonacciGoal: FibonacciGoal,
+};
